2e36571d1b8bfaaa60cf0390b3ea63669d7c5941
hiidy <hiidy115@gmail.com>
2024-03-04 19:53:25 +0900

    Pawn의 색깔 타입을 enum으로 변경
    - Pawn의 색 type을 enum으로 변경해서 다른 값이 할당되는 상황을 방지함.
    - 이에 따라서 Pawn 테스트 코드의 input값도 enum으로 변경했습니다.

---
+++ src/main/java/Piece/Pawn.java
@@ -0,0 +1,21 @@
+package Piece;
+
+public class Pawn {
+
+    public enum Color {BLACK, WHITE}
+
+    private final Color color;
+
+    public Pawn(Color color) {
+        this.color = color;
+    }
+
+    public Pawn() {
+        this.color = Color.WHITE;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+}
--- src/test/java/PawnTest.java
+++ src/test/java/PawnTest.java
@@ -1,18 +1,22 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.jupiter.api.*;
+import Piece.Pawn;
+import Piece.Pawn.Color;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 
 class PawnTest {
+
     @Test
     @DisplayName("pawn 생성 테스트")
     void createPawn() {
 
-        verifyPawn("white");
-        verifyPawn("black");
+        verifyPawn(Color.WHITE);
+        verifyPawn(Color.BLACK);
     }
 
-    void verifyPawn(final String color) {
+    void verifyPawn(Color color) {
         Pawn pawn = new Pawn(color);
         assertThat(pawn.getColor()).isEqualTo(color);
     }
@@ -21,6 +25,6 @@ class PawnTest {
     @DisplayName("pawn 기본 생성자 테스트")
     void createDefaultConstructor() {
         Pawn pawn = new Pawn();
-        assertThat(pawn.getColor()).isEqualTo("white");
+        assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 }
