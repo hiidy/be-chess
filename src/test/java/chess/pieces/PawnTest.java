cca4b371403b776d2ebf569e41049b614756fe01
hiidy <hiidy115@gmail.com>
2024-03-05 14:53:33 +0900

    Pawn의 color symbol이 잘 할당되었는지 테스트하는 기능 추가
    - 흑백 pawn을 각각 생성 했고 'P' 와 'p'로 잘 할당되었는지 테스트 했습니다.

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -13,7 +13,6 @@ class PawnTest {
     @Test
     @DisplayName("흰색과 검은색을 할당해서 Pawn 생성기능 테스트")
     void createPawn() {
-
         verifyPawn(Color.WHITE);
         verifyPawn(Color.BLACK);
     }
@@ -28,5 +27,14 @@ class PawnTest {
     void createDefaultConstructor() {
         Pawn pawn = new Pawn();
         assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
+    }
+
+    @Test
+    @DisplayName("pawn의 symbol은 color가 하얀색이면 p 검은색이면 P로 할당한다")
+    void verifySymbol() {
+        Pawn whitePawn = new Pawn(Color.WHITE);
+        assertThat(whitePawn.getSymbol()).isEqualTo('p');
+        Pawn blackPawn = new Pawn(Color.BLACK);
+        assertThat(blackPawn.getSymbol()).isEqualTo('P');
     }
 }
