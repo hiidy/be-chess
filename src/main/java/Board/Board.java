0532c26fe11a0898ae7a3bd3f9b1eddd42e1d536
hiidy <hiidy115@gmail.com>
2024-03-05 16:15:31 +0900

    처음 체스판에서 하얀 Pawn과 검은 Pawn을 만드는 기능 추가
    - 체스 룰에 따라서 2열과 7열에 각각 흰색 Pawn과 검은색 Pawn 8개를 배치했습니다.
    - 기물의 Position 정보를 2차원 배열에 담아서 관리했습니다.
    - 체스 보드의 사이즈나 Pawn의 초기 위치 정보는 불변성을 위해서 상수로 선언했습니다.

--- src/main/java/Board/Board.java
+++ src/main/java/Board/Board.java
@@ -3,12 +3,25 @@ package board;
 import java.util.ArrayList;
 import java.util.List;
 import pieces.Pawn;
+import pieces.Pawn.Color;
 
 public class Board {
 
     private static final int MAXIMUM_PAWN_NUMBER = 8;
+    private static final int CHESS_BOARD_SIZE = 8;
+    private static final int PAWN_INIT_POSITION = 1;
 
     private final List<Pawn> pieces = new ArrayList<>();
+    private Pawn[][] piecePositions = new Pawn[8][8];
+
+    public Board() {
+        initialize();
+    }
+
+    public void initialize() {
+        initializeWhitePawn();
+        initializeBlackPawn();
+    }
 
     public void addPawn(Pawn pawn) {
         checkPawnCount();
@@ -28,4 +41,21 @@ public class Board {
             throw new IllegalStateException("최대 폰 개수를 초과하였습니다.");
         }
     }
+
+    private void initializeBlackPawn() {
+        for (int i = 0; i < MAXIMUM_PAWN_NUMBER; i++) {
+            Pawn pawn = new Pawn(Color.BLACK);
+            pieces.add(pawn);
+            piecePositions[PAWN_INIT_POSITION][i] = pawn;
+        }
+    }
+
+    private void initializeWhitePawn() {
+        for (int i = 0; i < MAXIMUM_PAWN_NUMBER; i++) {
+            Pawn pawn = new Pawn(Color.WHITE);
+            pieces.add(pawn);
+            piecePositions[CHESS_BOARD_SIZE - 1 - PAWN_INIT_POSITION][i] = pawn;
+        }
+    }
+
 }
