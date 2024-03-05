3f4b0541501751bf5e248f99244eca9799142833
hiidy <hiidy115@gmail.com>
2024-03-05 11:37:40 +0900

    체스판 테스트 코드를 리팩터링
    - 체스판의 사이즈를 확인하고 Pawn을 찾는 메서드를 분리해서 테스트 단위를 작게 만들었습니다.

--- src/test/java/chess/board/BoardTest.java
+++ src/test/java/chess/board/BoardTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import board.Board;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pieces.Pawn;
@@ -11,31 +12,37 @@ import pieces.Pawn.Color;
 
 class BoardTest {
 
-    @Test
-    @DisplayName("체스판을 만들고 폰을 추가한 후 판의 사이즈와 find기능을 테스트")
-    void createChessBoard() {
-        Board board = new Board();
+    private Board board;
 
+    @BeforeEach
+    void setUp() {
+        board = new Board();
+    }
+
+
+    @Test
+    @DisplayName("폰을 추가하고 판의 사이즈를 확인")
+    void addPawnAndCheckSize() {
         Pawn white = new Pawn(Color.WHITE);
         board.addPawn(white);
         assertEquals(1, board.getBoardSize());
-        assertEquals(white, board.findPawn(0));
-
-        Pawn black = new Pawn(Color.BLACK);
-        board.addPawn(black);
-        assertEquals(2, board.getBoardSize());
-        assertEquals(black, board.findPawn(1));
     }
+
+    @Test
+    @DisplayName("폰을 추가하고 찾는 기능 테스트")
+    void addPawnAndFind() {
+        Pawn white = new Pawn(Color.WHITE);
+        board.addPawn(white);
+        assertEquals(white, board.findPawn(0));
+    }
+
 
     @Test
     @DisplayName("최대 폰 개수 초과 테스트")
     void exceedMaximumPawnNumber() {
-        Board board = new Board();
-
         for (int i = 0; i < 8; i++) {
             board.addPawn(new Pawn());
         }
-
         assertEquals(8, board.getBoardSize());
         assertEquals("최대 폰 개수를 초과하였습니다.", assertThrows(IllegalStateException.class,
             () -> board.addPawn(new Pawn())).getMessage());
