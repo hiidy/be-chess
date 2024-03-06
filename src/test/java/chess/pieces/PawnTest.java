b49bf036489bcf94ebefb52b112a122174bfed17
hiidy <hiidy115@gmail.com>
2024-03-06 18:32:32 +0900

    기호가 할당 되었는지 확인하는 테스트 코드를 리팩터링
    - 기존 메서드가 너무 많은 책임을 진다고 생각해서 메서드를 분리했습니다.

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import chess.pieces.Pawn.Color;
 
 
 class PawnTest {
@@ -24,16 +23,21 @@ class PawnTest {
     @Test
     @DisplayName("pawn 기본 생성자가 흰색 폰으로 생성되는지 테스트")
     void createDefaultConstructor() {
-        Pawn pawn = new Pawn();
+        Piece pawn = new Pawn();
         assertThat(pawn.getColor()).isEqualTo(Color.WHITE);
     }
 
     @Test
-    @DisplayName("pawn의 symbol은 color가 하얀색이면 p 검은색이면 P로 할당한다")
-    void verifySymbol() {
-        Pawn whitePawn = new Pawn(Color.WHITE);
-        assertThat(whitePawn.getSymbol()).isEqualTo('p');
-        Pawn blackPawn = new Pawn(Color.BLACK);
-        assertThat(blackPawn.getSymbol()).isEqualTo('P');
+    @DisplayName("흰색 Pawn일 때 ♟기호가 할당되는지 테스트합니다.")
+    void verifyWhitePawnSymbol() {
+        Piece whitePawn = new Pawn(Color.WHITE);
+        assertThat(whitePawn.getSymbol()).isEqualTo("♟");
+    }
+
+    @Test
+    @DisplayName("검은색 Pawn일 때 ♙기호가 할당되는지 테스트합니다.")
+    void verifyBlackPawnSymbol() {
+        Piece blackPawn = new Pawn(Color.BLACK);
+        assertThat(blackPawn.getSymbol()).isEqualTo("♙");
     }
 }
