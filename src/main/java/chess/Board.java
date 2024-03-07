3c2cabb4687f4876c0325f4ff50751ec4c98e290
hiidy <hiidy115@gmail.com>
2024-03-07 11:30:23 +0900

    Board 클래스의 전체적인 로직 수정
    - 기존에는 2차원 배열로 체스기물과 위치를 매핑했지만 그렇게 되면 추후에 움직임을 구현할 때 기물에 너무 많은 책임이 부여된다고 생각했습니다. 따라서 위치에 대한 클래스를 따로 만들고 Map으로 위치와 piece를 1대1 매핑했습니다.

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,56 +1,77 @@
 package chess;
 
-import chess.pieces.Pawn;
-import chess.pieces.Pawn.Color;
-import java.util.ArrayList;
-import java.util.List;
+import chess.pieces.Color;
+import chess.pieces.Piece;
+import chess.pieces.PieceFactory;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Board {
 
-    private static final int MAXIMUM_PAWN_NUMBER = 16;
     private static final int CHESS_BOARD_SIZE = 8;
-    private static final int WHITE_PAWN_INIT_POSITION = 6;
-    private static final int BLACK_PAWN_INIT_POSITION = 1;
 
-    private final List<Pawn> pieces = new ArrayList<>();
-    private Pawn[][] piecePositions = new Pawn[CHESS_BOARD_SIZE][CHESS_BOARD_SIZE];
+    private final Map<Position, Piece> piecePositions = new HashMap<>();
 
-
-    public void initialize() {
-        initializePawns(Color.WHITE, WHITE_PAWN_INIT_POSITION);
-        initializePawns(Color.BLACK, BLACK_PAWN_INIT_POSITION);
+    public void initializeChessBoard() {
+        initializePawn();
+        initializeKnight();
+        initializeBishop();
+        initializeRook();
+        initializeQueen();
+        initializeKing();
     }
 
-    public void addPawn(Pawn pawn) {
-        validateMaximumPawnNumber();
-        pieces.add(pawn);
+    private void initializePawn() {
+        for (int i = 1; i <= 8; i++) {
+            piecePositions.put(new Position(2, i), PieceFactory.createPiece("pawn", Color.WHITE));
+        }
+        for (int i = 1; i <= 8; i++) {
+            piecePositions.put(new Position(7, i), PieceFactory.createPiece("pawn", Color.BLACK));
+        }
+    }
+
+    private void initializeKnight() {
+        piecePositions.put(new Position(1, 2), PieceFactory.createPiece("knight", Color.WHITE));
+        piecePositions.put(new Position(1, 7), PieceFactory.createPiece("knight", Color.WHITE));
+        piecePositions.put(new Position(8, 2), PieceFactory.createPiece("knight", Color.BLACK));
+        piecePositions.put(new Position(8, 7), PieceFactory.createPiece("knight", Color.BLACK));
+    }
+
+    private void initializeBishop() {
+        piecePositions.put(new Position(1, 3), PieceFactory.createPiece("bishop", Color.WHITE));
+        piecePositions.put(new Position(1, 6), PieceFactory.createPiece("bishop", Color.WHITE));
+        piecePositions.put(new Position(8, 3), PieceFactory.createPiece("bishop", Color.BLACK));
+        piecePositions.put(new Position(8, 6), PieceFactory.createPiece("bishop", Color.BLACK));
+    }
+
+    private void initializeRook() {
+        piecePositions.put(new Position(1, 1), PieceFactory.createPiece("rook", Color.WHITE));
+        piecePositions.put(new Position(1, 8), PieceFactory.createPiece("rook", Color.WHITE));
+        piecePositions.put(new Position(8, 1), PieceFactory.createPiece("rook", Color.BLACK));
+        piecePositions.put(new Position(8, 8), PieceFactory.createPiece("rook", Color.BLACK));
+    }
+
+    private void initializeQueen() {
+        piecePositions.put(new Position(1, 4), PieceFactory.createPiece("queen", Color.WHITE));
+        piecePositions.put(new Position(8, 4), PieceFactory.createPiece("queen", Color.BLACK));
+    }
+
+    private void initializeKing() {
+        piecePositions.put(new Position(1, 5), PieceFactory.createPiece("king", Color.WHITE));
+        piecePositions.put(new Position(8, 5), PieceFactory.createPiece("king", Color.BLACK));
     }
 
     public int getPiecesNumber() {
-        return pieces.size();
+        return piecePositions.size();
     }
 
-    public Pawn findPawn(int pawnIndex) {
-        return pieces.get(pawnIndex);
-    }
-
-    private void validateMaximumPawnNumber() {
-        if (getPiecesNumber() == MAXIMUM_PAWN_NUMBER) {
-            throw new IllegalStateException("최대 폰 개수를 초과하였습니다.");
-        }
-    }
-
-    private void initializePawns(Color color, int pawnPosition) {
-        for (int i = 0; i < MAXIMUM_PAWN_NUMBER / 2; i++) {
-            Pawn pawn = new Pawn(color);
-            addPawn(pawn);
-            piecePositions[pawnPosition][i] = pawn;
-        }
+    public Piece findPiece(Position position) {
+        return piecePositions.get(position);
     }
 
     public String getChessBoardResult() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < CHESS_BOARD_SIZE; i++) {
+        for (int i = 1; i <= CHESS_BOARD_SIZE; i++) {
             sb.append(getRowResult(i));
         }
         return sb.toString();
@@ -58,18 +79,18 @@ public class Board {
 
     private String getRowResult(int row) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < CHESS_BOARD_SIZE; i++) {
+        for (int i = 1; i <= CHESS_BOARD_SIZE; i++) {
             sb.append(getPieceSymbol(row, i));
         }
         sb.append('\n');
         return sb.toString();
     }
 
-    private char getPieceSymbol(int row, int col) {
-        if (piecePositions[row][col] != null) {
-            return piecePositions[row][col].getSymbol();
+    private String getPieceSymbol(int row, int col) {
+        if (!piecePositions.containsKey(new Position(row, col))) {
+            return ".";
         }
-        return '.';
+        return piecePositions.get(new Position(row, col)).getSymbol();
     }
 
 }
